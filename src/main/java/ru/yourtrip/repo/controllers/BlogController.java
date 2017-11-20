91f6f7dfee0933d7f5f9b07fabec0bdb30d2a860
SaenkoDmitry <dmitryssaenko@gmail.com>
2017-11-20 13:57:50 +0300

    added partial add route

--- src/main/java/ru/yourtrip/repo/controllers/BlogController.java
+++ src/main/java/ru/yourtrip/repo/controllers/BlogController.java
@@ -2,10 +2,10 @@ package ru.yourtrip.repo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import ru.yourtrip.repo.models.Person;
 import ru.yourtrip.repo.models.Route;
@@ -38,20 +38,27 @@ public class BlogController {
     }
 
     @GetMapping(value="/creatingRoute")
-    public ModelAndView publishRoutePage(/*String username*/) {
+    public ModelAndView publishRoutePage(/*@RequestParam("username") String username*/) {
 //        Person person = personRepository.findByLogin(username);
         ModelAndView modelAndView = new ModelAndView("createroute");
-//        modelAndView.addObject("person_id", person.getId());
+        Route route = new Route();
+        modelAndView.addObject("route", route);
         return modelAndView;
     }
 
     @PostMapping(value="/creatingRoute", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public void publishRoute(Route route) {
+    public ModelAndView publishRoute(@ModelAttribute Route route) {
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+        String id = ((User)auth.getPrincipal()).getUsername();
+        route.setPerson_id(personRepository.getOne(Long.parseLong(id)));
         try {
             routeRepository.save(route);
         } catch (Exception ex) {
             System.out.println(ex);
         }
+        return new ModelAndView("index");
+
     }
 }
